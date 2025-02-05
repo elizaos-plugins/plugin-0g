798e6119485acbc0b71dba4fd4dcc20061ebdaf6
Carlos V <carlosv.provoos@gmail.com>
2025-02-05 16:00:27 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import type { Plugin } from "@elizaos/core";
 import { zgUpload } from "./actions/upload";
 
-export const zgPlugin: Plugin = {
+export const zgPlugin = {
     description: "ZeroG Plugin for Eliza",
     name: "ZeroG",
     actions: [zgUpload],
