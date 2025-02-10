24f2909eaf829cfc9c90336430b938d76405f506
Carlos V <carlosv.provoos@gmail.com>
2025-02-10 17:51:06 -0500

    chore: update tsconfig and tsup config

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { zgUpload } from "./actions/upload";
-
-export const zgPlugin = {
-    description: "ZeroG Plugin for Eliza",
-    name: "ZeroG",
-    actions: [zgUpload],
-    evaluators: [],
-    providers: [],
-};
